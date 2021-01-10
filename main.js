117b250dca415326079226f06e69a627c28e08ab
SamvegTiwari <74610526+SamvegTiwari@users.noreply.github.com>
2021-01-10 20:38:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log=("My first console project")
+var fname=["Samveg"];
+console.log=(fname)
+var Lname=["Tiwari"]  ;
+console.log=(Lname);
+var Fullname = fname.concat(lname);
+
+console.log(Fullname);
+
